81b92eb82ea1181e71d065dd48d9b648374d646c
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2023-03-13 09:03:29 -0300

    Change isPerfectTree() method in binaryTree class (#2)
    - renamed getDepth() to #calculateDepth()
    - now #calculateDepth() only goes from root to the first leaf on the left

--- code/index.js
+++ code/index.js
@@ -164,12 +164,12 @@ async function runBinaryTree() {
   // binaryTree.printInOrder();
   // console.log('traverseInOrder');
   // binaryTree.traverseInOrder();
-  console.log('printPostOrder');
-  binaryTree.printPostOrder();
-  console.log('traversePostOrder');
-  binaryTree.traversePostOrder();
+  // console.log('printPostOrder');
+  // binaryTree.printPostOrder();
+  // console.log('traversePostOrder');
+  // binaryTree.traversePostOrder();
   // console.log('FullTree:', binaryTree.isFullTree());
-  // console.log('PerfectTree:', binaryTree.isPerfectTree());
+  console.log('PerfectTree:', binaryTree.isPerfectTree());
   // console.log('TotalEdges:', binaryTree.getTotalEdges());
   // console.log('NodeHeight:', binaryTree.getNodeHeight(second));
   // console.log('TreeHeight:', binaryTree.getTreeHeight());
